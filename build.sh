4ee726ed0f1b1218d57e7888f0b59b5423c210e7
Minero Aoki <minero-aoki@cookpad.com>
2022-01-11 22:12:42 +0900

    disabling cache pull

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
-#!/bin/bash -eu
+#!/bin/bash -eux
 
 REPO=789035092620.dkr.ecr.ap-northeast-1.amazonaws.com/prism-base
 VERSION=3.0.3
 
-docker pull "${REPO}:latest-${VERSION}"
+# For cache
+#docker pull "${REPO}:latest-${VERSION}" || true
+
 docker build --build-arg "HADOOP_VERSION=${VERSION}" --tag "${REPO}:latest-${VERSION}" --tag "${REPO}:$(cat REVISION)-${VERSION}" --cache-from "${REPO}:latest-${VERSION}" .
+
 docker push "$REPO:$(cat REVISION)-$VERSION"
 docker push "$REPO:latest-$VERSION"
