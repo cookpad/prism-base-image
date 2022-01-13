1a8b7cbd46b04c067ae92674ed2a706109a85bb5
Minero Aoki <minero-aoki@cookpad.com>
2022-01-14 00:15:11 +0900

    Use VERSION-OS style Docker image tag

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -eux
 
-REPO=789035092620.dkr.ecr.ap-northeast-1.amazonaws.com/prism-base
 VERSION=3.0.3
+OS=buster
 
 # For cache
-#docker pull "${REPO}:latest-${VERSION}" || true
+#docker pull "$REPO:$VERSION-$OS" || true
 
-docker build --build-arg "HADOOP_VERSION=${VERSION}" --tag "${REPO}:latest-${VERSION}" --tag "${REPO}:$(cat REVISION)" --cache-from "${REPO}:latest-${VERSION}" .
+docker build --build-arg "HADOOP_VERSION=$VERSION" --tag "$REPO:$VERSION-$OS" --tag "$REPO:$(cat REVISION)" --cache-from "$REPO:$VERSION-$OS" .
 
 docker push "$REPO:$(cat REVISION)"
-docker push "$REPO:latest-$VERSION"
+docker push "$REPO:$VERSION-$OS"
