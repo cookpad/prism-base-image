ba6050c0e1cbe993e33ad75436f290a64fb14a69
Minero Aoki <minero-aoki@cookpad.com>
2022-01-19 21:53:33 +0900

    new: Upgrade Debian to bullseye

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM public.ecr.aws/docker/library/openjdk:11-jdk-buster
+FROM public.ecr.aws/docker/library/openjdk:11-jre-slim-bullseye
 
 WORKDIR /app
 
--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -eux
 
 VERSION=3.0.3
-OS=buster
+OS=bullseye
 
-# For cache
-#docker pull "$REPO:$VERSION-$OS" || true
+VERSION_TAG="${VERSION}-${OS}"
+REVISION_TAG="$(git rev-parse HEAD)"
 
-docker build --build-arg "HADOOP_VERSION=$VERSION" --tag "$REPO:$VERSION-$OS" --tag "$REPO:$(cat REVISION)" --cache-from "$REPO:$VERSION-$OS" .
+docker build --build-arg "HADOOP_VERSION=$VERSION" --tag "$VERSION_TAG" --tag "$REVISION_TAG" --cache-from "$VERSION_TAG" .
 
-docker push "$REPO:$(cat REVISION)"
-docker push "$REPO:$VERSION-$OS"
+docker push "$BASE_REPOS:$VERSION_TAG"
+docker push "$BASE_REPOS:$REVISION_TAG"
